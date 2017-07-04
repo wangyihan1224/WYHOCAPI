8628f983f4f3f80825f4b8fa35b91a34ac66bc2f
wangyihan <572466520@qq.com>
2017-07-04 17:56:25 +0800

    nsarray

--- Objective-C/Home/ViewController/AppFrameworks/Framework/WatchKit/NSArrayViewController.m
+++ Objective-C/Home/ViewController/AppFrameworks/Framework/WatchKit/NSArrayViewController.m
@@ -9,15 +9,77 @@
 #import "NSArrayViewController.h"
 
 @interface NSArrayViewController ()
-
+@property(nonatomic,strong)UIScrollView *wyhScrollView;
+@property(nonatomic,strong)UIView *arrayView;
+@property(nonatomic,strong)UIView *secondView;
+@property(nonatomic,strong)UIView *threeView;
 @end
 
 @implementation NSArrayViewController
-
+-(UIScrollView *)wyhScrollView{
+    
+    if (!_wyhScrollView) {
+        _wyhScrollView = [[UIScrollView alloc]init];
+        [_wyhScrollView sd_addSubviews:@[self.arrayView]];
+        
+        self.arrayView.sd_layout
+        .topSpaceToView(_wyhScrollView, 10)
+        .leftEqualToView(_wyhScrollView)
+        .rightEqualToView(_wyhScrollView);
+        
+        
+        [_wyhScrollView setupAutoContentSizeWithBottomView:self.arrayView bottomMargin:10];
+    }
+    return _wyhScrollView;
+}
+-(UIView *)arrayView{
+    if (!_arrayView) {
+        _arrayView = [UIView new];
+        
+        UILabel *titleLb = [UILabel new];
+        titleLb.text = @"+ (instancetype)array;";
+        [_arrayView addSubview:titleLb];
+        titleLb.sd_layout
+        .topEqualToView(_arrayView)
+        .leftEqualToView(_arrayView)
+        .autoHeightRatio(0);
+        [titleLb setSd_maxWidth:@(SCREENMAINWIDTH)];
+        
+        
+        UILabel *deslb = [UILabel new];
+        deslb.text = @"创建并返回一个空数组。\n例子：NSArray *array = [NSArray array];";
+        [_arrayView addSubview:deslb];
+        deslb.sd_layout
+        .topSpaceToView(titleLb, 10)
+        .leftEqualToView(_arrayView)
+        .autoHeightRatio(0);
+        [deslb setSd_maxWidth:@(SCREENMAINWIDTH)];
+        
+        [_arrayView setupAutoHeightWithBottomView:deslb bottomMargin:10];
+    }
+    return _arrayView;
+}
+-(UIView *)secondView{
+    if (!_secondView) {
+        _secondView = [UIView new];
+    }
+    return _secondView;
+}
+-(UIView *)threeView{
+    
+    if (!_threeView) {
+        _threeView = [UIView new];
+    }
+    return _threeView;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self.view addSubview:self.wyhScrollView];
+    self.wyhScrollView.sd_layout.spaceToSuperView(UIEdgeInsetsZero);
     // Do any additional setup after loading the view.
 }
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
