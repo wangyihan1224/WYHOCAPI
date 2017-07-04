5b078130952d453d6b6e8ebc8838256be35ad1b7
wangyihan <572466520@qq.com>
2017-07-04 16:05:15 +0800

    AppFrameworks

--- Objective-C/Home/ViewController/AppFrameworks/AppFrameworksViewController.m
+++ Objective-C/Home/ViewController/AppFrameworks/AppFrameworksViewController.m
@@ -7,17 +7,65 @@
 //
 
 #import "AppFrameworksViewController.h"
-
-@interface AppFrameworksViewController ()
-
+#import "AppKitViewController.h"
+#import "FoundationViewController.h"
+#import "ObjectiveCViewController.h"
+#import "UIKitViewController.h"
+#import "WatchKitViewController.h"
+static NSString *appKitViewController=@"AppKitViewController";
+static NSString *foundationViewController=@"FoundationViewController";
+static NSString *objectiveCViewController=@"ObjectiveCViewController";
+static NSString *uIKitViewController=@"UIKitViewController";
+static NSString *watchKitViewController=@"WatchKitViewController";
+static NSString *templateCell = @"templateCell";
+@interface AppFrameworksViewController ()<UITableViewDelegate,UITableViewDataSource>
+@property(nonatomic,strong)UITableView *wyhTable;
+@property(nonatomic,strong)NSMutableArray *dataArray;
 @end
 
 @implementation AppFrameworksViewController
 
+-(UITableView *)wyhTable{
+    
+    if (!_wyhTable) {
+        _wyhTable = [[UITableView alloc]initWithFrame:CGRectMake(0, 0, SCREENMAINWIDTH, SCREENMAINHEIGHT) style:UITableViewStylePlain];
+        _wyhTable.delegate = self;
+        _wyhTable.dataSource = self;
+        _wyhTable.separatorStyle = UITableViewCellSeparatorStyleNone;
+        [_wyhTable registerClass:[UITableViewCell class] forCellReuseIdentifier:templateCell];
+        
+    }
+    return _wyhTable;
+}
+-(NSMutableArray *)dataArray{
+    if (!_dataArray) {
+        _dataArray = [NSMutableArray new];
+        [_dataArray addObject:appKitViewController];
+        [_dataArray addObject:foundationViewController];
+        [_dataArray addObject:objectiveCViewController];
+        [_dataArray addObject:uIKitViewController];
+        [_dataArray addObject:watchKitViewController];
+    }
+    return _dataArray;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self.view addSubview:self.wyhTable];
     // Do any additional setup after loading the view.
 }
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    return self.dataArray.count;
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:templateCell];
+    cell.textLabel.text = self.dataArray[indexPath.row];
+    return cell;
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
---
+++ Objective-C/Home/ViewController/AppFrameworks/Framework/Objective-C/ObjectiveCViewController.h
@@ -0,0 +1,13 @@
+//
+//  ObjectiveCViewController.h
+//  Objective-C
+//
+//  Created by 王乙涵 on 2017/7/4.
+//  Copyright © 2017年 simpleway. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface ObjectiveCViewController : BaseViewController
+
+@end
