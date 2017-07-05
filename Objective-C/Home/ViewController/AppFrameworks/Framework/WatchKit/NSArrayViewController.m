d30076c6b9b0243a2c58e031793a61534902f33f
wangyihan <572466520@qq.com>
2017-07-05 11:29:11 +0800

    nsarray

--- Objective-C/Home/ViewController/AppFrameworks/Framework/WatchKit/NSArrayViewController.m
+++ Objective-C/Home/ViewController/AppFrameworks/Framework/WatchKit/NSArrayViewController.m
@@ -55,16 +55,25 @@
         .autoHeightRatio(0);
         [deslb setSd_maxWidth:@(SCREENMAINWIDTH)];
         
+        UIView *line = [UIView new];
+        line.backgroundColor = REDCOLOR;
+        [_arrayView addSubview:line];
+        line.sd_layout
+        .leftEqualToView(_arrayView)
+        .rightEqualToView(_arrayView)
+        .heightIs(1)
+        .topSpaceToView(deslb, 10);
+      //............................................................................................
         UILabel *titleLb2 = [UILabel new];
         titleLb2.text = @"+ (instancetype)arrayWithArray:(NSArray<ObjectType> *)array;";
         [_arrayView addSubview:titleLb2];
         titleLb2.sd_layout
-        .topSpaceToView(deslb, 10)
+        .topSpaceToView(line, 10)
         .leftEqualToView(_arrayView)
         .autoHeightRatio(0);
         [titleLb2 setSd_maxWidth:@(SCREENMAINWIDTH)];
         
-        
+       
         
         UILabel *deslb2 = [UILabel new];
         deslb2.text = @"创建并返回包含在另一个给定数组中的对象的数组。\n例子：NSArray *array1 = [NSArray new];\nNSArray *array2 = [NSArray arrayWithArray:array1];";
@@ -75,7 +84,47 @@
         .autoHeightRatio(0);
         [deslb2 setSd_maxWidth:@(SCREENMAINWIDTH)];
         
-        [_arrayView setupAutoHeightWithBottomView:deslb2 bottomMargin:10];
+        UIView *line2 = [UIView new];
+        line2.backgroundColor = REDCOLOR;
+        [_arrayView addSubview:line2];
+        line2.sd_layout
+        .leftEqualToView(_arrayView)
+        .rightEqualToView(_arrayView)
+        .heightIs(1)
+        .topSpaceToView(deslb2, 10);
+        //............................................................................................
+        UILabel *titleLb3 = [UILabel new];
+        titleLb3.text = @"+ (NSArray<ObjectType> *)arrayWithContentsOfFile:(NSString *)path;";
+        [_arrayView addSubview:titleLb3];
+        titleLb3.sd_layout
+        .topSpaceToView(line2, 10)
+        .leftEqualToView(_arrayView)
+        .autoHeightRatio(0);
+        [titleLb3 setSd_maxWidth:@(SCREENMAINWIDTH)];
+        
+        
+        
+        UILabel *deslb3 = [UILabel new];
+        deslb3.text = @"创建并返回一个包含给定路径指定文件内容的数组。组。\n例子：NSString *nsarray = [[NSBundle mainBundle] pathForResource:@\"NSArrayEmple\" ofType:@\"plist\"];\nNSArray *arr = [NSArray arrayWithContentsOfFile:nsarray];";
+        [_arrayView addSubview:deslb3];
+        deslb3.sd_layout
+        .topSpaceToView(titleLb3, 10)
+        .leftEqualToView(_arrayView)
+        .autoHeightRatio(0);
+        [deslb3 setSd_maxWidth:@(SCREENMAINWIDTH)];
+        
+        UIView *line3 = [UIView new];
+        line3.backgroundColor = REDCOLOR;
+        [_arrayView addSubview:line3];
+        line3.sd_layout
+        .leftEqualToView(_arrayView)
+        .rightEqualToView(_arrayView)
+        .heightIs(1)
+        .topSpaceToView(deslb3, 10);
+        
+         //............................................................................................
+        
+        [_arrayView setupAutoHeightWithBottomView:line3 bottomMargin:10];
     }
     return _arrayView;
 }
